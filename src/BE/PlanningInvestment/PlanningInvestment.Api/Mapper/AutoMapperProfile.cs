d0e5103d809c1607f0b07ba45c6484573204d5bf
VinhLTT <>
2025-06-07 19:22:51 +0700

    feat: Add initial project structure and core domain entities for PlanningInvestment
    - Created IFilterBodyRequest interface for filtering requests.
    - Implemented LoginDto for user login information.
    - Established PlanningInvestment.Contracts project with necessary dependencies.
    - Introduced IBaseRepository interface for generic repository operations.
    - Developed Debt entity with properties for debt management.
    - Defined AccountType, DebtType, GoalStatus, and InvestmentType enums for categorization.
    - Set up PlanningInvestment.Domain project with essential configurations.
    - Added IUnitOfWork interface for managing transactions and repositories.
    - Established PlanningInvestment.Infrastructure project with basic setup.
    - Created solution file to organize the PlanningInvestment projects.

---
+++ src/BE/PlanningInvestment/PlanningInvestment.Api/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using PlanningInvestment.Application.DTOs.ExpectedTransaction;
+using PlanningInvestment.Application.DTOs.RecurringTransactionTemplate;
+using PlanningInvestment.Domain.Entities;
+using PlanningInvestment.Domain.Enums;
+
+namespace CoreFinance.Application.Mapper;
+
+/// <summary>
+    /// (EN) AutoMapper profile for mapping between domain entities and DTOs.<br/>
+    /// (VI) Cấu hình AutoMapper để ánh xạ giữa các entity miền (domain) và DTO.
+/// </summary>
+public class AutoMapperProfile : Profile
+{
+    /// <summary>
+    /// (EN) Initializes a new instance of the <see cref="AutoMapperProfile"/> class.<br/>
+    /// (VI) Khởi tạo một phiên bản mới của lớp <see cref="AutoMapperProfile"/>.
+/// </summary>
+    public AutoMapperProfile()
+    {
+        CreateMap<ExpectedTransaction, ExpectedTransactionCreateRequest>().ReverseMap();
+        CreateMap<ExpectedTransaction, ExpectedTransactionUpdateRequest>().ReverseMap();
+        CreateMap<ExpectedTransaction, ExpectedTransactionViewModel>()
+            .ForMember(dest => dest.TemplateName,
+                opt => opt.MapFrom(src =>
+                    src.RecurringTransactionTemplate != null ? src.RecurringTransactionTemplate.Name : string.Empty))
+            .ForMember(dest => dest.AccountName,
+                opt => opt.MapFrom(src => src.Account != null ? src.Account.Name : string.Empty))
+            .ForMember(dest => dest.AccountType,
+                opt => opt.MapFrom(src => src.Account != null ? (AccountType?)src.Account.Type : null))
+            .ForMember(dest => dest.HasActualTransaction, opt => opt.MapFrom(src => src.ActualTransactionId.HasValue))
+            .ForMember(dest => dest.DaysUntilDue,
+                opt => opt.MapFrom(src => (int)(src.ExpectedDate - DateTime.UtcNow).TotalDays))
+            .ReverseMap()
+            .ForMember(dest => dest.RecurringTransactionTemplate, opt => opt.Ignore())
+            .ForMember(dest => dest.Account, opt => opt.Ignore())
+            .ForMember(dest => dest.ActualTransaction, opt => opt.Ignore());
+        CreateMap<RecurringTransactionTemplate, RecurringTransactionTemplateCreateRequest>().ReverseMap();
+        CreateMap<RecurringTransactionTemplate, RecurringTransactionTemplateUpdateRequest>().ReverseMap();
+        CreateMap<RecurringTransactionTemplate, RecurringTransactionTemplateViewModel>()
+            .ForMember(dest => dest.AccountName,
+                opt => opt.MapFrom(src => src.Account != null ? src.Account.Name : string.Empty))
+            .ForMember(dest => dest.AccountType,
+                opt => opt.MapFrom(src => src.Account != null ? (AccountType?)src.Account.Type : null))
+            .ForMember(dest => dest.ExpectedTransactionCount, opt => opt.MapFrom(src => 0)) // Default value for test
+            .ReverseMap()
+            .ForMember(dest => dest.Account, opt => opt.Ignore());
+    }
+}
